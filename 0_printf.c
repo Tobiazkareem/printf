e9eea86b56e327f5d84486f7eedd0940369ed11c
Tobiazkareem <Tobiazkareem@gmail.com>
2023-09-17 21:29:02 +0000

    printf project task 0

--- 0_printf.c
+++ 0_printf.c
@@ -11,10 +11,8 @@ int _printf(const char *format, ...)
 {
 	int write_character_number = 0;
 	char character_c;
-	va_list write_args_format; /* declare va_list to add as many arguments*/
+	va_list write_args_format;
 
-	if (format == NULL) /* check to see if the character supplied is acceptable */
-		return (-1); /* return an error -1 */
 	va_start(write_args_format, format); /* set arguments into list */
 	while (*format) /*iterates through the character */
 	{
@@ -26,31 +24,34 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++; /*move to next character */
-			/* checking next character to see if it is a null character */
-			if (*format == '\0')
-				break; /* if true, break the loop and end */
-		if (*format == '%')/* checking for conversion specifiers c, s and % */
-		{
-			write(1, format, 1);
-			write_character_number++; /* increment numbers of character printed */
-		}
-		else if (*format == 'c')
-		{
+			if (*format == 'c') /* check if character is c, s or % */
+			{
 			character_c = va_arg(write_args_format, int);
 			write(1, &character_c, 1);
 			write_character_number++;
+			}
+			else if (*format == 's')
+			{
+				write_character_number += _print_string(va_arg(write_args_format, char *));
 		}
-		else if (*format == 's')
-		{
-			write_character_number += _print_string(va_arg(write_args_format, char *));
-		}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				write_character_number++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				write_character_number++;
+				write(1, format, 1);
+				write_character_number++;
+			}
 		}
 		format++;
-		}
+	}
 	va_end(write_args_format);
 	return (write_character_number);
 }
-
 /**
  * _print_string - print string
  * @string: string to print out
