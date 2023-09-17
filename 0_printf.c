b46838dd5e6f02accd90b427dd844d3819a8e174
Tobiazkareem <Tobiazkareem@gmail.com>
2023-09-17 23:40:04 +0000

    printf project task 0

--- 0_printf.c
+++ 0_printf.c
@@ -1,79 +1,150 @@
 #include "main.h"
 
+int _printf(const char *format, ...);
+
 /**
- * _printf - functions that produces output according to a format
- * @format: character format that determines the specifier c, s or %
+ * _putchar - writes character
+ * @c: character
+ * Return: number of charaters written
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_character - character to print
+ * @argument: contains character to print
  *
- * Return: Return the number of characters printed
+ * Return: number of char printed
+ */
+
+int print_character(va_list argument)
+{
+	/* declare variable character */
+	char character;
+
+	character = va_arg(argument, int);
+	return (_putchar(character));
+}
+
+/**
+ * string_print - print string
+ * @argument: va_list that contains the string to print
+ *
+ * Return: number of characters printed
+ */
+
+int string_print(va_list argument)
+{
+	/* declare variable strings */
+	int count1 = 0;
+	char *strings;
+
+	strings = va_arg(argument, char *);
+	/* check is string is NULL */
+	if (strings == NULL)
+		strings = "(null)";
+
+	while (*strings)
+	{
+		_putchar(*strings);
+		count1++;
+		strings++;
+	}
+	return (count1);
+}
+
+/**
+ * int_print - int print
+ * @argument: va_list that contains the integer to print
+ *
+ * Return: number of characters printed
+ */
+
+int int_print(va_list argument)
+{
+	/* declare variable */
+	int number1, number_reverse = 0, count2 = 0;
+
+	number1 = va_arg(argument, int);
+
+	if (number1 < 0)
+	{
+		_putchar('-');
+		count2++;
+		number1 = -number1;
+	}
+	if (number1 == 0)
+	{
+		_putchar('0');
+		count2++;
+	}
+	else
+	{
+		while (number1 != 0)
+		{
+			number_reverse = number_reverse * 10 + number1 % 10;
+			number1 /= 10;
+		}
+		while (number_reverse != 0)
+		{
+			_putchar('0' + number_reverse % 10);
+			count2++;
+			number_reverse /= 10;
+		}
+	}
+	return (count2);
+}
+
+/**
+ * _printf - printf custom function
+ * @format: format string
+ *
+ * Return: number of characters returned
  */
 
 int _printf(const char *format, ...)
 {
-	int write_character_number = 0;
-	char character_c;
-	va_list write_args_format;
+	/* declare variables */
+	va_list argument;
+	int count3 = 0;
 
-	va_start(write_args_format, format); /* set arguments into list */
-	while (*format) /*iterates through the character */
+	va_start(argument, format); /* set argument into the va_list */
+
+	while (*format) /* iterate the format string */
 	{
-		if (*format != '%') /* if not % sign, write charater to stdout */
+		if (*format == '%')
 		{
-			write(1, format, 1);
-			write_character_number++;
-		}
-		else
-		{
-			format++; /*move to next character */
-			if (*format == 'c') /* check if character is c, s or % */
-			{
-			character_c = va_arg(write_args_format, int);
-			write(1, &character_c, 1);
-			write_character_number++;
-			}
+			format++;
+			if (*format == '\0')
+				break; /* discontinue and end loop if format is null byte */
+			if (*format == 'c')
+				count3 += print_character(argument);
 			else if (*format == 's')
-			{
-				write_character_number += _print_string(va_arg(write_args_format, char *));
-			}
+				count3 += string_print(argument);
+			else if (*format == 'd' || *format == 'i')
+				count3 += int_print(argument);
 			else if (*format == '%')
 			{
-				write(1, format, 1);
-				write_character_number++;
+				_putchar('%');
+				count3++;
 			}
 			else
 			{
-				write(1, "%", 1);
-				write_character_number++;
-				write(1, format, 1);
-				write_character_number++;
+				_putchar('%');
+				_putchar(*format);
+				count3 += 2;
 			}
+		}
+		else
+		{
+			_putchar(*format);
+			count3++;
 		}
 		format++;
 	}
-	va_end(write_args_format);
-	return (write_character_number);
-}
-/**
- * _print_string - print string
- * @string: string to print out
- *
- * Return: numbers of printed characters
- */
-
-int _print_string(char *string)
-{
-	int count_string = 0;
-
-	if (string == NULL)
-	{
-		string = "(null)";
-	}
-
-	while (*string)
-	{
-		write(1, string, 1);
-		count_string++;
-		string++;
-	}
-
-	return (count_string);
+	va_end(argument);
+	return (count3);
 }
--- main.h
+++ main.h
@@ -1,16 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
+
 
 /**
- * prototypes for printf project
- * Add here...
+ * _printf - prinf custom function
+ * @format: string to format
+ *
+ * Return: return the number of characters
  */
 
 int _printf(const char *format, ...);
-int _print_string(char *string);
+int int_print(va_list argument);
+int string_print(va_list argument);
+int print_character(va_list argument);
+int _putchar(char c);
 
 #endif
